fd675e555e16c798bb2e13ca681d1757599d60fc
Sergio Barreras <sergiobg.isc@gmail.com>
2024-05-02 07:52:36 -0700

    flows and configs

---
+++ Controllers/PostController.cs
@@ -0,0 +1,22 @@
+﻿using Backend.DTOs;
+using Backend.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        private IPostService _postService;
+
+        public PostController(IPostService postService)
+        {
+            _postService = postService;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<PostDto>> Get() => await _postService.Get();
+    }
+}
